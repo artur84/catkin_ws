6b46d95916ed124ae4e1a941f97526ab7e2827af
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-04-21 16:09:45 -0500

    Adding fixed_tf_broadcaster, some comments previous to my robotics lesson

--- src/learning_tf/src/learning_tf/turtle_tf_broadcaster.py
+++ src/learning_tf/src/learning_tf/turtle_tf_broadcaster.py
@@ -8,17 +8,19 @@ import turtlesim.msg
 
 def handle_turtle_pose(msg, turtlename):
     br = tf.TransformBroadcaster()
-    br.sendTransform((msg.x, msg.y, 0),
+    br.sendTransform((msg.x, msg.y, 0), 
                      tf.transformations.quaternion_from_euler(0, 0, msg.theta),
-                     rospy.Time.now(),
-                     turtlename,
-                     "world")
+                     rospy.Time.now(), turtlename, "world")
+    print 'I received a message:'
+    print msg
+    print 'turtlename:'
+    print turtlename
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
-    turtlename = 'turtle1'
-    rospy.Subscriber('/%s/pose' % turtlename,
-                     turtlesim.msg.Pose,
-                     handle_turtle_pose,
-                     turtlename)
+    #rospy.get_param('~nombre_parametro', 'valor por default')
+    turtlename = rospy.get_param('~turtle', 'turtle1')
+    print 'The node started'
+    rospy.Subscriber('/%s/pose' % turtlename, turtlesim.msg.Pose, handle_turtle_pose, turtlename)
     rospy.spin()
+    
--- src/learning_tf/src/learning_tf/turtle_tf_listener.py
+++ src/learning_tf/src/learning_tf/turtle_tf_listener.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python  
+# -*- coding: utf-8 -*-
 import roslib
 roslib.load_manifest('learning_tf')
 import rospy
@@ -9,22 +10,29 @@ import turtlesim.srv
 
 if __name__ == '__main__':
     rospy.init_node('tf_turtle')
-
+    
+    #Crea la estructura para escuchar transformaciones
     listener = tf.TransformListener()
-
+    
+    # Crea una tortuga nueva en el simulador (turtle2)
+    # Recordar cuando usamos rosservice call spawn 2 2 0.2 ""
     rospy.wait_for_service('spawn')
     spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
     spawner(4, 2, 0, 'turtle2')
-
+    
+    # Crea un publicador de velocidad para la tortuga 2 (Mover turtle2)
     turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
-
+    
+    # Fija la frecuancia a la cual va a publicar turtle_vel
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
+            #Obtiene la traslación y rotación de /turtle2 con respecto a /turtle1
             (trans,rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+            #rospy.Time(0) última transformación enviada.
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-
+        #Controla la velocidad de turtle2 proporcionalmente a la distancia lineal y ángular
         angular = 4 * math.atan2(trans[1], trans[0])
         linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
         cmd = geometry_msgs.msg.Twist()
