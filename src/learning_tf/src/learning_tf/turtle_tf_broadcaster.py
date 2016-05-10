a33f1ad3f84b7261f18aebb7c2914b952d701171
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-05-09 19:26:44 -0500

    Delete some prints from turtle_tf_broadcaster.py

--- src/learning_tf/src/learning_tf/turtle_tf_broadcaster.py
+++ src/learning_tf/src/learning_tf/turtle_tf_broadcaster.py
@@ -11,16 +11,11 @@ def handle_turtle_pose(msg, turtlename):
     br.sendTransform((msg.x, msg.y, 0), 
                      tf.transformations.quaternion_from_euler(0, 0, msg.theta),
                      rospy.Time.now(), turtlename, "world")
-    print 'I received a message:'
-    print msg
-    print 'turtlename:'
-    print turtlename
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
     #rospy.get_param('~nombre_parametro', 'valor por default')
     turtlename = rospy.get_param('~turtle', 'turtle1')
-    print 'The node started'
     rospy.Subscriber('/%s/pose' % turtlename, turtlesim.msg.Pose, handle_turtle_pose, turtlename)
     rospy.spin()
     
